a9cd0b01c09afb65feadb3cb3f5eb98c16b3795b
maye <ssdd050@163.com>
2018-05-03 16:11:08 +0800

    Initial commit

---
+++ app/src/main/java/com/maye/example/HomeActivity.java
@@ -0,0 +1,40 @@
+package com.maye.example;
+
+import android.animation.ValueAnimator;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.animation.LinearInterpolator;
+
+import com.maye.satelitelayout.SatelliteLayout;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private SatelliteLayout sl_home;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        initComponent();
+    }
+
+    private void initComponent() {
+        sl_home = findViewById(R.id.sl_home);
+
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, 360);
+        valueAnimator.setInterpolator(new LinearInterpolator());
+        valueAnimator.setDuration(3000);
+        valueAnimator.setRepeatCount(2);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                float angle = (float) valueAnimator.getAnimatedValue();
+                Log.i("角度值", "角度：" + angle);
+                sl_home.setAngle(angle);
+            }
+        });
+        valueAnimator.start();
+    }
+}
